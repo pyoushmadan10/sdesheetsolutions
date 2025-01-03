5fef0162b1f6a4db63e38a053706ef066b19dfbe
Pyoush Madan <97949390+pyoushmadan10@users.noreply.github.com>
2025-01-03 22:47:24 +0530

    Time: 7 ms (27.35%), Space: 89.3 MB (62.32%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-    static int waysToSplitArray(vector<int>& nums) {
-        const int n=nums.size();
-        long long sum=accumulate(nums.begin(), nums.end(), 0LL);
-        long long acc=0LL;
-        int cnt=0;
-        for(int i=0; i<n-1; i++){
-            acc+=nums[i];
-            cnt+=(2*acc>=sum);
+    int waysToSplitArray(vector<int>& nums) {
+        // Keep track of sum of elements on left and right sides
+        long long leftSum = 0, rightSum = 0;
+
+        // Initially all elements are on right side
+        for (int num : nums) {
+            rightSum += num;
         }
-        return cnt;
+
+        int count = 0;
+        // Try each possible split position
+        for (int i = 0; i < nums.size() - 1; i++) {
+            // Move current element from right to left side
+            leftSum += nums[i];
+            rightSum -= nums[i];
+
+            // Check if this creates a valid split
+            if (leftSum >= rightSum) {
+                count++;
+            }
+        }
+
+        return count;
     }
 };
