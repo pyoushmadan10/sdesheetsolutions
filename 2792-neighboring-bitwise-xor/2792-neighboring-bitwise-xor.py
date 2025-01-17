1b8a14bb860a5fd00033a76f65c295f75548148d
Pyoush Madan <97949390+pyoushmadan10@users.noreply.github.com>
2025-01-17 23:37:37 +0530

    Time: 52 ms (49.63%), Space: 22.4 MB (48.9%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def doesValidArrayExist(self, derived):
+        n = len(derived)
+        xor_sum = 0    
+        for num in derived:
+            xor_sum ^= num
+        
+        return xor_sum == 0
+        
