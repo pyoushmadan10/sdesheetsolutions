87b404e15bf6a74cc3b737865669e753f133942e
Pyoush Madan <97949390+pyoushmadan10@users.noreply.github.com>
2025-03-13 23:37:05 +0530

    Time: 169 ms (73.93%), Space: 63.2 MB (68.09%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minZeroArray(self, nums, queries):
+        n = len(nums)
+        sum_value = 0
+        query_count = 0
+        diff_array = [0] * (n + 1)
+        for i in range(n):
+            while sum_value + diff_array[i] < nums[i]:
+                query_count += 1
+                if query_count > len(queries):
+                    return -1
+                left, right, value = queries[query_count - 1]
+                if right >= i:
+                    diff_array[max(left, i)] += value
+                    if right + 1 < len(diff_array):
+                        diff_array[right + 1] -= value
+            sum_value += diff_array[i]
+        return query_count
