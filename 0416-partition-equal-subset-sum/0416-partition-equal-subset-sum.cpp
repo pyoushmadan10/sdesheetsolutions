6c1e2f7c36bd286b92333de730f8063dda8fc64b
Pyoush Madan <97949390+pyoushmadan10@users.noreply.github.com>
2025-04-07 14:41:27 +0530

    Time: 63 ms (86.66%), Space: 13.4 MB (90.95%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& arr) {
+        int total = accumulate(arr.begin(), arr.end(), 0);
+        // If total is odd, can't split into two equal subsets
+        if (total % 2 != 0) return false;
+
+        int sum = total / 2;
+        int n = arr.size();
+
+        vector<bool> prev(sum + 1, false), curr(sum + 1, false);
+        prev[0] = true;  // base case: sum 0 is always possible
+
+        for (int i = 1; i <= n; i++) {
+            curr[0] = true;  // sum = 0 is always achievable
+            for (int j = 1; j <= sum; j++) {
+                if (j < arr[i - 1])
+                    curr[j] = prev[j];
+                else
+                    curr[j] = prev[j] || prev[j - arr[i - 1]];
+            }
+            prev = curr;
+        }
+
+        return prev[sum];  // just return true or false
+    }
+};
