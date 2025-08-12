2d9925afe9e86c794b8aaced64480b4bdc7a1bc8
Pyoush Madan <97949390+pyoushmadan10@users.noreply.github.com>
2025-08-12 23:02:39 +0530

    Time: 278 ms (89.83%), Space: 17.8 MB (93.02%) - LeetHub

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        MOD = 1_000_000_007  
+        powers = []
+        i = 1
+        while True:
+            p = i ** x
+            if p > n:
+                break
+            powers.append(p)
+            i += 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for p in powers:
+            for s in range(n, p - 1, -1):
+                dp[s] = (dp[s] + dp[s - p]) % MOD
+
+        return dp[n]
